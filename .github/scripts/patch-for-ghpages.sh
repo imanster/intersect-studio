29e9341e8daea085193883fe468e14aab131e9af
Navneet Karnani <navneet@mandraketech.in>
2026-02-08 18:28:29 +0530

    patch data too

--- .github/scripts/patch-for-ghpages.sh
+++ .github/scripts/patch-for-ghpages.sh
@@ -11,8 +11,10 @@ fi
 "${SED_INPLACE[@]}" 's/BrowserRouter/HashRouter/g' src/App.tsx
 
 # Patch absolute asset paths to relative for subpath deployment
-find src -type f \( -name '*.tsx' -o -name '*.ts' -o -name '*.jsx' -o -name '*.js' -o -name '*.css' \) \
-  -exec "${SED_INPLACE[@]}" 's|src="/|src="./|g; s|href="/|href="./|g; s|url("/|url("./|g; s|url(/|url(./|g' {} +
+SRC_FILES=$(find src -type f \( -name '*.tsx' -o -name '*.ts' -o -name '*.jsx' -o -name '*.js' -o -name '*.css' \))
 
-# Patch index.html absolute paths
-"${SED_INPLACE[@]}" 's|href="/|href="./|g; s|src="/|src="./|g' index.html
+"${SED_INPLACE[@]}" 's|src="/|src="./|g' $SRC_FILES index.html
+"${SED_INPLACE[@]}" 's|href="/|href="./|g' $SRC_FILES index.html
+"${SED_INPLACE[@]}" 's|url("/|url("./|g' $SRC_FILES
+"${SED_INPLACE[@]}" 's|url(/|url(./|g' $SRC_FILES
+"${SED_INPLACE[@]}" 's|url: "/|url: "./|g' $SRC_FILES
